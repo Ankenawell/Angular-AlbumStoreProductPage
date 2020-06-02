95e7d515d1af396f128dea87cba627daa854d38b
Adam Kenawell <51209269+Ankenawell@users.noreply.github.com>
2020-06-02 09:02:01 -0400

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -9,12 +9,12 @@ import {ProductService} from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albuminfo;
+  albumInfo;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albuminfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
